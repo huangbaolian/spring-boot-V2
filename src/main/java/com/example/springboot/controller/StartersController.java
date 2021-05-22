d8dd3252da49ff2a0dbfa8fe7832b492671e8f11
hbl <824428286@qq.com>
2021-05-23 00:37:09 +0800

    starter

---
+++ src/main/java/com/example/springboot/controller/StartersController.java
@@ -0,0 +1,17 @@
+package com.example.springboot.controller;
+
+import com.demo.starter.service.DemoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class StartersController {
+    @Resource
+    private DemoService demoService;
+    @GetMapping("/say")
+    public String getStarterTest(){
+       return demoService.say();
+    }
+}
